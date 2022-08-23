753a2ee92f14ebd127476b66a669025014ba9bc7
Alex Contreras <alexcontreraspizarro@gmail.com>
2022-08-23 16:50:28 -0400

    primer commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return <>
+      <div class="classFooter">
+        <h5>Renderización dinámica de componentes</h5>
+        <h6>Alex Contreras</h6>
+      </div>
+    </>
+  }
+  
+  export default Footer;
